49e355c53f23c47c33940dcd56de22c215f547a2
jc428732 <connor.chivers@my.jcu.edu.au>
2017-08-16 13:25:33 +1000

    Add name printing program.

--- oddName.py
+++ oddName.py
@@ -1,3 +1,9 @@
 """
 Connor Chivers
 """
+
+name = str(input("What is your name?"))
+if len(name) > 0:
+    for letter in range(0, len(name)):
+        if letter % 2 == 1:
+            print(name[letter])
